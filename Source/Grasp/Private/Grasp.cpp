27c001eb5b4bcf07d0369d5083980ef1248d7aa0
Jared Taylor <jaredtaylor.99@gmail.com>
2025-04-20 16:10:05 +1200

    initial commit

---
+++ Source/Grasp/Private/Grasp.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Grasp.h"
+
+#define LOCTEXT_NAMESPACE "FGraspModule"
+
+void FGraspModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FGraspModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FGraspModule, Grasp)
